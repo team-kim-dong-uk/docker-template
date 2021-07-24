d96366319429ee83cf18fb5f8dab61fb46ef694a
jooomOut <rimeilo324@naver.com>
2021-07-24 16:56:10 +0900

    UPDATE HiController
    get server ip for checking lb

--- src/main/java/com/example/demo/HiController.java
+++ src/main/java/com/example/demo/HiController.java
@@ -4,10 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HiController {
     @GetMapping()
     public String hi(){
-        return "Hi~UPDATE22222!!";
+        InetAddress local = null;
+        try {
+            local = InetAddress.getLocalHost();
+        }
+        catch ( UnknownHostException e ) {
+            e.printStackTrace();
+        }
+
+        if( local == null ) {
+            return "can not get ip";
+        }
+        else {
+            String ip = local.getHostAddress();
+            return "Hi! server is "+ ip ;
+        }
+
     }
 }
